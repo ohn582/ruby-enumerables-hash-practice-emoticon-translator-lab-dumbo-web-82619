be48d7b15cec9cae56e8f2ec1fa75ffdc4e0d7e1
Neal JIn Oh <no4259@gmail.com>
2019-08-17 02:20:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  lib = YAML.load_file(path)
+  lib_2 = Hash.new
+
+  lib_2["get_emoticon"] = Hash.new
+  lib_2["get_meaning"] = Hash.new
+
+  lib.each do |word, emotpic|
+    lib_2["get_emoticon"][emotpic.first] = emotpic.last
+    lib_2["get_meaning"][emotpic.last] = word
+  end
+  lib_2
 end
 
 def get_japanese_emoticon
